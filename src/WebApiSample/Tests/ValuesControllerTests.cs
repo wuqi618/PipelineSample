2faa01a76f83ef5a87609f05f71cd71d0c79171b
Nelson Wu <nelson.wu@xero.com>
2017-11-01 15:12:19 +1100

    add WebApiSample

---
+++ src/WebApiSample/Tests/ValuesControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WebApiSample.Controllers;
+using Xunit;
+
+namespace Tests
+{
+    public class ValuesControllerTests
+    {
+        [Fact]
+        public void GetShouldSucceed()
+        {
+            var valuesController = new ValuesController();
+            var result = valuesController.Get();
+            Assert.Equal(2, result.Count());
+        }
+    }
+}
