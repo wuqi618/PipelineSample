db6effaec64c0006d34ab49a6b63c2226b25bf0b
Nelson Wu <nelson.wu@xero.com>
2018-03-22 14:22:10 +1100

    add HomeController.cs and Dockerfile.ci

---
+++ src/WebApiSample/WebApiSample/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiSample.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "WebApiSample";
+        }
+    }
+}
